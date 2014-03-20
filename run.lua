db2f6697caa1e811afb6947dc1b2dce2901fab3b
outsky <317525370@qq.com>
2014-03-20 20:03:32 +0800

    add test csv

--- run.lua
+++ run.lua
@@ -1,14 +1,7 @@
 require("plua")
 csv  = require('csv')
-tab = csv.load('MapEctypeNormal.csv', ',')
-for k,v in ipairs(tab) do
-    print(k, v)
-    for kk,vv in ipairs(v) do
-        print(kk,vv)
-    end
-end
-
+tab = csv.load('csv/MapEctypeNormal.csv', ',')
 local str = plua(tab)
 
-local f = io.open("out.lua", "w")
+local f = io.open("lua/MapEctypeNormal.lua", "w")
 f:write(str)
